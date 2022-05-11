6fd6cc4e67d19329ba4a4384b2ffeb0e07822296
NasyrovAR <80483338+NasyrovAR@users.noreply.github.com>
2022-05-12 00:09:19 +0500

    2 лаба

---
+++ 2/index.php
@@ -0,0 +1,14 @@
+<html>
+<head> <title> 2 лаба</title> </head>
+<body>
+<h1>Лабораторная работа №2 Насыров А.Р. ПИ-323</h1><p>
+<u1>
+        <li><a href="http://a0670959.xsph.ru/2/2-1.html"> 1 упражнение</a></li>
+        <li><a href="http://a0670959.xsph.ru/2/2-2.html"> 2 упражнение</a></li>
+        <li><a href="http://a0670959.xsph.ru/2/2-3.html"> 3 упражнение</a></li>
+        <li><a href="http://a0670959.xsph.ru/2/2-4.html"> 4 упражнение</a></li>
+        <li><a href="http://a0670959.xsph.ru/2/2-5.html"> 5 упражнение</a></li>
+        <li><a href="http://a0670959.xsph.ru/2/2-6.html"> 6 упражнение</a></li>
+        <li><a href="http://a0670959.xsph.ru/2/2-7.html"> 7 упражнение</a></li>
+</u1>
+</html>
