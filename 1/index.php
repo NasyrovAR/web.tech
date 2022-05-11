772e20207234c7cf47f348ab1d974eb1f61463ec
NasyrovAR <80483338+NasyrovAR@users.noreply.github.com>
2022-05-11 23:12:07 +0500

    1 лаба

---
+++ 1/index.php
@@ -0,0 +1,14 @@
+<html>
+<head> <title> 1 лаба</title> </head>
+<body>
+<h1>Лабораторная работа №1 Насыров А.Р. ПИ-323</h1><p>
+<u1>
+        <li><a href="http://a0670959.xsph.ru/1/Lab1.html"> 1 упражнение</a></li>
+        <li><a href="http://a0670959.xsph.ru/1/Lab2.html"> 2 упражнение</a></li>
+        <li><a href="http://a0670959.xsph.ru/1/Lab3.html"> 3 упражнение</a></li>
+        <li><a href="http://a0670959.xsph.ru/1/Lab4.html"> 4 упражнение</a></li>
+        <li><a href="http://a0670959.xsph.ru/1/Lab5.html"> 5 упражнение</a></li>
+        <li><a href="http://a0670959.xsph.ru/1/Lab6.html"> 6 упражнение</a></li>
+        <li><a href="http://a0670959.xsph.ru/1/Lab7.html"> 7 упражнение</a></li>
+</u1>
+</html>
